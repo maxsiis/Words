27afb3d5eff067e1e3bdbc53738b2c6b6b53e6a8
maxsiis <maksim.aleksandrov.2016@bk.ru>
2025-08-31 22:16:49 +0300

    добавил запрос к api

--- src/App.vue
+++ src/App.vue
@@ -5,50 +5,72 @@ import Card from './components/Card/Card.vue';
 import { computed, ref } from 'vue';
 
 const score = ref(100);
-const data = ref([
-  {
-    word: 'Carom',
-    translation: 'Свинец',
-    state: false,
-    status: 'pending',
-  },
-  {
-    word: 'Car',
-    translation: 'Автомобиль',
-    state: true,
-    status: 'pending',
-  },
-  {
-    word: 'Duck',
-    translation: 'Утка',
-    state: true,
-    status: 'success',
-  },
-  {
-    word: 'Foreman',
-    translation: 'Бригадир',
-    state: true,
-    status: 'failed',
-  },
-]);
+const data = ref();
+const error = ref();
+
+const displayGame = ref(false);
+const displayButton = ref(true);
+
+function showGame() {
+  displayGame.value = true;
+  displayButton.value = false;
+  console.log('карты показаны');
+}
 
 function FlipCard(card) {
   console.log(card);
+}
+
+const dataModified = computed(() => {
+  if (!data.value) return [];
+  return [
+    {
+      word: data.value.word,
+      translation: data.value.translation,
+      state: false,
+      status: 'pending',
+    },
+    {
+      word: data.value.word,
+      translation: data.value.translation,
+      state: true,
+      status: 'success',
+    },
+    {
+      word: data.value.word,
+      translation: data.value.translation,
+      state: true,
+      status: 'failed',
+    },
+  ];
+});
+async function getWord() {
+  const res = await fetch('http://localhost:8080/api/random-words');
+  if (res.status != 200) {
+    data.value = null;
+    error.value = await res.json();
+  }
+  error.value = null;
+  data.value = await res.json();
 }
 </script>
 
 <template>
   <div>
-    <Card
-      v-for="(card, index) in data"
-      :key="index"
-      v-bind="card"
-      @flipCard="FlipCard"
-    />
+    <div class="card-items" v-if="displayGame">
+      <Card
+        v-for="item in dataModified"
+        :key="item.word"
+        :word="item.word"
+        :translation="item.translation"
+        :state="item.state"
+        :status="item.status"
+      />
+    </div>
     <div>
       <Score :score="score" />
       <div class="app-container">
-        <Button>Начать игру</Button>
+        <Button v-if="displayButton" @click="showGame">Начать игру</Button>
       </div>
     </div>
   </div>
